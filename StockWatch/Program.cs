1a449144d4ff4831e3a71fee8e1aae6b7a1c7fb5
Lucca Augusto Torres Tambor <lucca.tambor@rotadasoficinas.com.br>
2023-08-05 17:22:55 -0300

    feat: use args to select stock and define sell and buy values
    now when running the program the user must insert the arguments of the stock to be monitored and the sell and buy values

--- StockWatch/Program.cs
+++ StockWatch/Program.cs
@@ -9,8 +9,16 @@ namespace StockWatch
     public class Program {
         static void Main(string[] args) {
             Console.WriteLine("[StockWatch]: Bem vindo ao programa de observação de ativos");
-            Configuration? configuration = new Configuration();
+            string stockName = args[0];
+            float sellValue = float.Parse(args[1]);
+            float buyValue = float.Parse(args[2]);
 
+            Console.WriteLine($"[StockWatch]: O ativo a ser monitorado é: {stockName}");
+            Console.WriteLine($"[StockWatch]: Valor de venda: R$ {sellValue}");
+            Console.WriteLine($"[StockWatch]: Valor de compra: R$ {buyValue}");
+
+            Configuration? configuration = new Configuration();
+           
             var serializer = new JsonSerializer();
             using(var streamReader = new StreamReader("config.json"))
             using(var textReader = new JsonTextReader(streamReader)) {
@@ -25,14 +33,16 @@ namespace StockWatch
             HttpClient httpClient = new HttpClient();
 
             try {
-                var test = httpClient.GetAsync("https://brapi.dev/api/quote/MGLU3?range=1d&interval=1d&fundamental=true&dividends=true").Result;
-                var jsonString = test.Content.ReadAsStringAsync().Result;
-                BrapiApiResponse brapiApiResponse = JsonConvert.DeserializeObject<BrapiApiResponse>(jsonString);
+                var response = httpClient.GetAsync($"https://brapi.dev/api/quote/{stockName}?range=1d&interval=1d&fundamental=true&dividends=true").Result;
+                var jsonResult = response.Content.ReadAsStringAsync().Result;
+                BrapiApiResponse brapiApiResponse = JsonConvert.DeserializeObject<BrapiApiResponse>(jsonResult);
                 Console.WriteLine($"[StockWatch]: O valor atual do ativo é de R$ {brapiApiResponse.Results.FirstOrDefault().RegularMarketPrice}");
             } 
             catch(Exception ex) {
                 Console.WriteLine($"[StockWatch]: ERRO - Erro ao acessar API de ativos");
             }
+
+            Console.ReadLine();
 
             //try {
             //    EmailSender.SendEmail(configuration, "Test Email", "Hey this is a test email from stock watch");
